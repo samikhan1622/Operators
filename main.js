05503dba64caf8faf9b33d87abd959a4ee86d208
samikhan1622 <153662652+samikhan1622@users.noreply.github.com>
2024-02-25 14:09:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+// //Q#01
+// console.log("hello sami");
+//Q#02  (define  var, let, const)
+// let myName = "sami";
+// let firstName = "ali";
+// let score = "25";
+// console.log(myName, firstName, score);
+//Q#03 (operators)
+//example#01 for numbers,
+// let n1 = 1
+// let n2 = 2
+// console.log(n1 + n2);
+//example#02 for strings.
+// let str1 = "1"
+// let str2 = "2"
+// console.log(str1 + str2);
+// //subtraction example,
+// let n3 = 5;
+// let n4 = 2;
+// console.log(n3 - n4); 
+// let str1 = "5";
+// let str2 = "6";
+// console.log(str1 + str2);
+// let qty = 4;
+// console.log( qty - 6);
+// console.log( qty + 3);
+// console.log( qty * 12000);
+// console.log( qty / 3);
+// let n1 = 2;
+// let n2 = 3;
+// console.log(n1 ** n2);
+// console.log( qty % 3);
+// let age = 22;
+// age += 10;
+// age -= 10;
+// console.log(age);
+//  let n = 5;
+//  console.log(n);
+//Describe ( if, else, & els if )
+// (if or else) example,
+var x = ("where does the people live?");
+var correctAnswer = "pakistan";
+if (x == correctAnswer) {
+    console.log("correct!");
+}
+else {
+    console.log("wrong!");
+}
+//  let powder = ("lal mirch powder");
+// powder = "haldi powder"
+// console.log("powder")
+// describe (var, let, & const)
+//  const fullName = "sami"
+// const shakeel = "shakell"
+// console.log(fullName, shakeel)
+//let fullName = "sami sherzaman"
+//  let fullName = "sami";
+// let lastName = "sherzaman";
+// console.log(fullName + " " + lastName)
+// console.log(`my nam is ${fullName} ${lastName}`);
+// let str1 = "1";
+//  let str2 = "2";
+//  console.log(str1 + str2);
+// let age = 22;
+// age += 10;
+// age -= 10;
+//  console.log(age);
+// let age = 22
+// console.log( age == 22);
